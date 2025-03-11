5590aa3afa82b642d966b83bcaf4023beb7e5e6d
Chadwick Koo <chadwickkoo@outlook.com>
2025-03-11 21:44:49 +0300

    modified index.js file

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution here
+    const users = [
+        {
+        name: "Niky",
+        hometown: "Dallas",
+        },
+        {
+        name: "Tracy",
+        hometown: "LA",
+        },
+        {
+        name: "Josh",
+        hometown: "Chicago",
+        },
+        {
+        name: "Kate",
+        hometown: "Orlando",
+        },
+        {
+        name: "Avidor",
+        hometown: "Philadelphia",
+        },
+        {
+        name: "Drew",
+        hometown: "Miami",
+        },
+    ];
+
+function findMatching(drivers, string){
+    return drivers.filter(function(driver) {
+        // Convert both driver and searchString to lowercase for case-insensitive comparison
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(function(driver) {
+        // Ouputs matching first initial
+        return driver.startsWith(string);
+    });
+}
+
+
+function matchName(drivers, string){
+    return drivers.filter(function(driver) {
+        return driver.name === string;
+})
+}
